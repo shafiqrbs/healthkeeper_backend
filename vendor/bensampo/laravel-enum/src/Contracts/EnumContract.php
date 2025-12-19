5834f2b3a7a9a32fcde2e2e19b2afe0ec18f2d69
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-12-19 23:31:28 +0600

    Remove VAT & production module.

--- vendor/bensampo/laravel-enum/src/Contracts/EnumContract.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace BenSampo\Enum\Contracts;
-
-interface EnumContract
-{
-    /** Determine if this instance is equivalent to a given value. */
-    public function is(mixed $enumValue): bool;
-}
--- vendor/bensampo/laravel-enum/src/Contracts/LocalizedEnum.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace BenSampo\Enum\Contracts;
-
-interface LocalizedEnum
-{
-    /** Get the default localization key. */
-    public static function getLocalizationKey(): string;
-}
