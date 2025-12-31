2aabd05be7f8c33f3db9e1555ee454cfc284448b
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-12-31 16:49:04 +0600

    Update local medicine IPD add mediicne form update.

--- Modules/Hospital/App/Http/Controllers/IpdPrescriptionController.php
+++ Modules/Hospital/App/Http/Controllers/IpdPrescriptionController.php
@@ -165,7 +165,7 @@ class IpdPrescriptionController extends Controller
     {
         foreach ($request->order as $row) {
             PatientPrescriptionMedicineModel::where('id', $row['id'])
-                ->update(['ordering' => $row['ordering']]);
+                ->update(['order' => $row['ordering']]);
         }
         return response()->json(['success' => true]);
     }
