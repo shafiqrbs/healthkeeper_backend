e6068beb23a0c372dac663fd249e70a0e7634404
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2026-02-24 13:05:21 +0600

    Medicine Module update

--- Modules/Medicine/App/Http/Requests/PurchaseRequest.php
+++ Modules/Medicine/App/Http/Requests/PurchaseRequest.php
@@ -21,6 +21,7 @@ class PurchaseRequest extends FormRequest
         return [
             'vendor_id' => 'required|integer|regex:/^\d+(\.\d{1,2})?$/',
             'created_by_id' => 'nullable|integer',
+            'received_date' => 'nullable|date',
             'grn' => 'nullable|string',
             'remark' => 'nullable|string',
             'items' => 'required|array',
